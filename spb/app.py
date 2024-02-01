9d315f2493a10b8e832b332ff83ea275079a5312
Jee Won Park <jeewon@nyriad.io>
2024-02-02 10:25:23 +1300

    Get jobs page working

--- spb/app.py
+++ spb/app.py
@@ -18,8 +18,8 @@ def getCursor():
     global dbconn
     global connection
     connection = mysql.connector.connect(user=connect.dbuser, \
-    password=connect.dbpass, host=connect.dbhost, \
-    database=connect.dbname, autocommit=True)
+                                         password=connect.dbpass, host=connect.dbhost, \
+                                         database=connect.dbname, autocommit=True)
     dbconn = connection.cursor()
     return dbconn
 
@@ -27,24 +27,29 @@ def getCursor():
 def home():
     return redirect("/currentjobs")
 
+def generate_href(field_value):
+    # Your logic to generate the href based on field_value
+    # For example, let's say you want to link to Google search using the field value
+    return f'/job/{field_value}'
+
 @app.route("/currentjobs")
 def currentjobs():
     connection = getCursor()
     # add customers' name combine togther, add customer table for query
     connection.execute("SELECT a.job_id,a.customer,concat(ifnull(b.first_name,''),' ',ifnull(b.family_name,'')) customer_name,a.job_date FROM job a, customer b where a.completed=0 and a.customer=b.customer_id;")
     jobList = connection.fetchall()
-    return render_template("currentjoblist.html", job_list = jobList)    
+    return render_template("currentjoblist.html", job_list=jobList, generate_href=generate_href)
 
 @app.route("/job/<int:job_id>")
 def job_detail(job_id):
     connection = getCursor()
-    
+
     # Fetch job details
     connection.execute("SELECT * FROM job WHERE job_id = %s", (job_id,))
     job = connection.fetchone()
 
     # From job_part, job and part table query the part usage of this job
-    connection.execute("SELECT a.*, b.qty FROM part a JOIN job_part b ON a.part_id = b.part_id WHERE a.job_id = %s;", (job_id,))
+    connection.execute("SELECT a.*, b.qty FROM part a JOIN job_part b ON a.part_id = b.part_id WHERE b.job_id = %s;", (job_id,))
     parts = connection.fetchall()
 
     # From job_service, service and job table query the service usage of this job
@@ -62,4 +67,3 @@ def job_detail(job_id):
 
 if __name__ == '__main__':
     app.run()
-
